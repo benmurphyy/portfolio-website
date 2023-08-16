245f6b0998b09335cac74496b556affb65ae7fda
benmurphyy <47269122+benmurphyy@users.noreply.github.com>
2023-08-16 18:30:36 +0800

    Finish 22/23 sem 2 updates

--- src/pages/home/components/AboutMe/index.tsx
+++ src/pages/home/components/AboutMe/index.tsx
@@ -23,11 +23,11 @@ const AboutMe = forwardRef((_, ref: ForwardedRef<HTMLDivElement>) => {
       <Row className="pt-4 justify-content-center">
         <Col className="align-items-center" lg={7} xl={6}>
           <p className="fs-4 text-center">
-            I'm Ben, a year 2 student in the National University of Singapore
-            (NUS), pursuing a Bachelor's degree (Hons) in Computer Science. I
-            created this website to be a space for me to share my technical
-            skills and industry experience, along with projects I've done.
-            Thanks for taking a look, and feel free to connect with me!
+            I'm Ben, a final year student in the National University of
+            Singapore (NUS), pursuing a Bachelor's degree (Hons) in Computer
+            Science. I created this website to be a space for me to share my
+            technical skills and industry experience, along with projects I've
+            done. Thanks for taking a look, and feel free to connect with me!
           </p>
         </Col>
       </Row>
